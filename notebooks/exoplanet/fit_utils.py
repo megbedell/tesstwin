214a9c3155615962ada58b85429547aed28c6d2c
Megan Bedell <megbedell@gmail.com>
2020-01-14 16:04:47 -0500

    attempting two-planet fit

---
+++ notebooks/exoplanet/fit_utils.py
@@ -0,0 +1,233 @@
+import numpy as np
+import pymc3 as pm
+import theano.tensor as tt
+import exoplanet as xo
+import matplotlib.pyplot as plt
+
+
+def build_model(x, y, yerr, x_rv, y_rv, yerr_rv, pug, pug_pred, t_rv, texp,
+                periods, t0s, depths, msini, mass_radius_mu, mass_radius_cov, 
+                mask=None, start=None, bis=False, bis_rv=None, 
+                fwhm=False, fwhm_rv=None, rv_trend_order=1):
+    n_pl = len(periods)
+    if rv_trend_order < 1:
+        assert False, "rv_trend_order must be >= 1"
+    if mask is None:
+        mask = np.ones(len(x), dtype=bool)
+    with pm.Model() as model:
+
+        # Stellar properties
+        mr_star = pm.Bound(pm.MvNormal, lower=0)(
+            "mr_star",
+            mu=mass_radius_mu,
+            chol=np.linalg.cholesky(mass_radius_cov),
+            shape=2,
+            testval=mass_radius_mu)
+        m_star = pm.Deterministic("m_star", mr_star[0])
+        r_star = pm.Deterministic("r_star", mr_star[1])
+        
+        mean_flux = pm.Normal("mean_flux", mu=0.0, sd=10.0)
+        u_star = xo.distributions.QuadLimbDark("u_star")
+
+        log_period = pm.Normal("log_period", mu=np.log(periods), sd=10.0, shape=n_pl)
+        period = pm.Deterministic("period", tt.exp(log_period))
+        t0 = pm.Uniform("t0", lower=t0s-1.0, upper=t0s+1.0, shape=n_pl)
+        
+        log_ror = pm.Uniform("log_ror", lower=np.log(0.01), upper=np.log(0.1),
+                             testval=0.5*np.log(1e-3*depths), shape=n_pl)
+        ror = pm.Deterministic("ror", tt.exp(log_ror))
+        b_hat = xo.distributions.UnitUniform("b_hat", shape=n_pl)
+        b = pm.Deterministic("b", b_hat * (1 + ror))
+        r_pl = pm.Deterministic("r_pl", ror * r_star)
+        
+        log_m_pl = pm.Bound(pm.Normal, lower=0.0)(
+            "log_m_pl", mu=np.log(msini.value), sd=5)
+        m_pl = pm.Deterministic("m_pl", tt.exp(log_m_pl))
+        
+        ecc = xo.distributions.eccentricity.kipping13("ecc", testval=np.zeros(n_pl)+0.01, shape=n_pl)
+        #ecc = xo.distributions.UnitUniform("ecc", testval=np.zeros(n_pl)+0.01, shape=n_pl)
+        omega = xo.distributions.Angle("omega", testval=np.zeros(n_pl), shape=n_pl)
+        
+        # Transit jitter & GP parameters
+        logs2 = pm.Normal("logs2", mu=np.log(np.var(y[mask])), sd=10)
+        logw0_guess = np.log(2*np.pi/10)
+        logw0 = pm.Normal("logw0", mu=logw0_guess, sd=10)
+
+        # We'll parameterize using the maximum power (S_0 * w_0^4) instead of
+        # S_0 directly because this removes some of the degeneracies between
+        # S_0 and omega_0
+        logpower = pm.Normal("logpower",
+                             mu=np.log(np.var(y[mask]))+4*logw0_guess,
+                             sd=10)
+        logS0 = pm.Deterministic("logS0", logpower - 4 * logw0)
+            
+        # RV jitter & an RV trend
+        logs_rv_pre = pm.Normal("logs_rv_pre", mu=np.log(np.median(yerr_rv[~pug])), sd=5)
+        logs_rv_pug = pm.Normal("logs_rv_pug", mu=np.log(np.median(yerr_rv[pug])), sd=5)
+        offset_pug = pm.Normal("offset_pug", mu=15.4, sd=0.4) # strong prior based on other solar twins
+        trend = pm.Normal("trend", mu=0, sd=10.0**(1-np.arange(rv_trend_order))[::-1], shape=rv_trend_order)
+        if bis:
+            offset_pug_bis = pm.Normal("offset_pug_bis", mu=1., sd=5) # weak prior from eyeballing
+            trend_bis = pm.Normal("trend_bis", mu=0, sd=10.0**(1-np.arange(2))[::-1], shape=2)
+        if fwhm:
+            offset_pug_fwhm = pm.Normal("offset_pug_fwhm", mu=1., sd=5) # weak prior from eyeballing
+            trend_fwhm = pm.Normal("trend_fwhm", mu=0, sd=10.0**(1-np.arange(2))[::-1], shape=2)
+            
+        # Orbit model
+        orbit = xo.orbits.KeplerianOrbit(
+            r_star=r_star, m_star=m_star,
+            period=period, t0=t0, b=b,
+            ecc=ecc, omega=omega,
+            m_planet=m_pl, 
+            m_planet_units=msini.unit)
+        
+        # Save some helpful things for later
+        semimajor = orbit.a
+        pm.Deterministic('a', semimajor)
+
+        # Compute the model light curve using starry
+        model.light_curves = xo.LimbDarkLightCurve(u_star).get_light_curve(
+            orbit=orbit, r=r_pl, t=x[mask], texp=texp)*1e3
+        model.light_curve = pm.math.sum(model.light_curves, axis=-1) + mean_flux
+
+        # GP model for the light curve
+        kernel = xo.gp.terms.SHOTerm(log_S0=logS0, log_w0=logw0, Q=1/np.sqrt(2))
+        model.gp = xo.gp.GP(kernel, x[mask], tt.exp(logs2) + tt.zeros(mask.sum()))
+        pm.Potential("transit_obs", model.gp.log_likelihood(y[mask] - model.light_curve))
+        model.gp_pred = model.gp.predict()
+
+        # Set up the RV model and save it as a deterministic
+        # for plotting purposes later
+        vrad = orbit.get_radial_velocity(x_rv)
+        pm.Deterministic("vrad", vrad)
+        
+        # Define the background RV model
+        A = np.vander(x_rv, rv_trend_order)
+        bkg_terms = pug*offset_pug + tt.dot(A, trend)
+        bkg_noactivity = pm.Deterministic("bkg_noactivity", bkg_terms)
+        if bis:
+            A_bis = np.vander(bis_rv, 2)
+            bkg_terms += tt.dot(A_bis, trend_bis) + pug*offset_pug_bis*trend_bis[0]
+        if fwhm:
+            A_fwhm = np.vander(fwhm_rv, 2)
+            bkg_terms += tt.dot(A_fwhm, trend_fwhm) + pug*offset_pug_fwhm*trend_fwhm[0]            
+        bkg = pm.Deterministic("bkg", bkg_terms)
+
+
+        # The likelihood for the RVs
+        if n_pl > 1:
+            rv_model = pm.Deterministic("rv_model", tt.sum(vrad, axis=-1) + bkg)
+        else:
+            rv_model = pm.Deterministic("rv_model", vrad + bkg)        
+        err_pre = tt.sqrt(yerr_rv[~pug]**2 + tt.exp(2*logs_rv_pre))
+        err_pug = tt.sqrt(yerr_rv[pug]**2 + tt.exp(2*logs_rv_pug))
+        pm.Normal("obs_pre", mu=rv_model[~pug], sd=err_pre, observed=y_rv[~pug])
+        pm.Normal("obs_post", mu=rv_model[pug], sd=err_pug, observed=y_rv[pug])
+        
+        vrad_pred = orbit.get_radial_velocity(t_rv)
+        pm.Deterministic("vrad_pred", vrad_pred)
+        A_pred = np.vander(t_rv, rv_trend_order)        
+        bkg_pred = pm.Deterministic("bkg_pred", pug_pred*offset_pug + tt.dot(A_pred, trend))
+        if n_pl > 1:
+            pm.Deterministic("rv_model_pred", tt.sum(vrad_pred, axis=-1) + bkg_pred)
+        else:
+            pm.Deterministic("rv_model_pred", vrad_pred + bkg_pred)
+
+        # Fit for the maximum a posteriori parameters, I've found that I can get
+        # a better solution by trying different combinations of parameters in turn
+        if start is None:
+            start = model.test_point
+        map_soln = start
+        map_soln = xo.optimize(start=map_soln, vars=[logs2])
+        map_soln = xo.optimize(start=map_soln, vars=[logs_rv_pre, logs_rv_pug, trend, offset_pug])
+        if bis:
+            map_soln = xo.optimize(start=map_soln, vars=[trend_bis, offset_pug_bis])
+        if fwhm:
+            map_soln = xo.optimize(start=map_soln, vars=[trend_fwhm, offset_pug_fwhm])
+        map_soln = xo.optimize(start=map_soln, vars=[log_ror, b_hat])
+        map_soln = xo.optimize(start=map_soln, vars=[log_period, t0])
+        map_soln = xo.optimize(start=map_soln, vars=[log_m_pl])
+        map_soln = xo.optimize(start=map_soln, vars=[logs2, logpower])
+        map_soln = xo.optimize(start=map_soln, vars=[logw0])
+        map_soln = xo.optimize(start=map_soln)
+
+    return model, map_soln
+
+def plot_rv_curve(soln, x_rv, y_rv, yerr_rv, pug, t_rv):
+    fig, axes = plt.subplots(2, 1, figsize=(10, 5), sharex=True)
+
+    ax = axes[0]
+    ax.errorbar(x_rv[~pug], y_rv[~pug], yerr=yerr_rv[~pug], fmt=".k")
+    ax.errorbar(x_rv[pug], y_rv[pug], yerr=yerr_rv[pug], fmt=".r")
+    ax.plot(t_rv, soln["vrad_pred"], "--k", alpha=0.5)
+    ax.plot(t_rv, soln["bkg_pred"], ":k", alpha=0.5)
+    ax.plot(t_rv, soln["rv_model_pred"], label="model")
+    ax.legend(fontsize=10)
+    ax.set_ylabel("radial velocity [m/s]")
+
+    ax = axes[1]
+    err_pre = np.sqrt(yerr_rv[~pug]**2+np.exp(2*soln["logs_rv_pre"]))
+    err_pug = np.sqrt(yerr_rv[pug]**2+np.exp(2*soln["logs_rv_pug"]))
+    ax.errorbar(x_rv[~pug], y_rv[~pug] - soln["rv_model"][~pug], yerr=err_pre, fmt=".k")
+    ax.errorbar(x_rv[pug], y_rv[pug] - soln["rv_model"][pug], yerr=err_pug, fmt=".k")
+    ax.axhline(0, color="k", lw=1)
+    ax.set_ylabel("residuals [m/s]")
+    ax.set_xlim(t_rv.min() - 50, t_rv.max() + 50)
+    ax.set_xlabel("time [days]")
+    return fig
+
+def plot_light_curve(model, soln, x, y, mask=None):
+    if mask is None:
+        mask = np.ones(len(x), dtype=bool)
+
+    fig, axes = plt.subplots(3, 1, figsize=(10, 7), sharex=True)
+    
+    with model:
+        gp_mod, lc_mod = xo.eval_in_model([
+            model.gp_pred + model.mean_flux, model.light_curve - model.mean_flux], soln)
+
+    ax = axes[0]
+    ax.plot(x[mask], y[mask], "k", label="data")
+    ax.plot(x[mask], gp_mod, color="C2", label="GP model")
+    ax.legend(fontsize=10, loc=3)
+    ax.set_ylabel("Relative Flux (ppt)", fontsize=12)
+
+    ax = axes[1]
+    ax.plot(x[mask], y[mask] - gp_mod, "k", label="de-trended data")
+    ax.plot(x[mask], lc_mod, label="transit model")
+    ax.legend(fontsize=10, loc=3)
+    ax.set_ylabel("De-trended Flux (ppt)", fontsize=12)
+
+    ax = axes[2]
+    mod = gp_mod + lc_mod
+    ax.plot(x[mask], y[mask] - mod, "k")
+    ax.axhline(0, color="#aaaaaa", lw=1)
+    ax.set_ylabel("Residuals (ppt)", fontsize=12)
+    ax.set_xlim(x[mask].min(), x[mask].max())
+    ax.set_xlabel("Time (days)")
+
+    return fig
+
+# useful functions for trace statistics:
+
+def percentiles(x):
+    # this fails for unknown indexing-related reasons
+    return pd.DataFrame(np.percentile(x, [0.15, 16, 50, 84, 99.85]), 
+                        columns=['3sigma_lower', '1sigma_lower', 'median', '1sigma_upper', '3sigma_upper'],
+                        dtype=np.float64)
+
+def median(x):
+    return pd.Series(np.percentile(x, [50]), name='median')
+
+def onesigma_upper(x):
+    return pd.Series(np.percentile(x, [84]), name='1sigma_upper')
+
+def onesigma_lower(x):
+    return pd.Series(np.percentile(x, [16]), name='1sigma_lower')
+
+def threesigma_upper(x):
+    return pd.Series(np.percentile(x, [99.85]), name='3sigma_upper')
+
+def threesigma_lower(x):
+    return pd.Series(np.percentile(x, [0.15]), name='3sigma_lower')
+    
